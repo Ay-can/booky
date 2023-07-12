d4716bbaa0b31fc9a0f1cebc809762b112bced7e
Aycan <aycan.acn2020@gmail.com>
2023-07-12 14:22:22 +0200

    Fix tablestate in a yucky way, do better later....

--- src/app.rs
+++ src/app.rs
@@ -8,7 +8,7 @@ pub type AppResult<T> = std::result::Result<T, Box<dyn error::Error>>;
 
 const JSON_PATH: &str = "../data/books.json";
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Book {
     pub id: usize,
     pub title: String,
@@ -19,45 +19,25 @@ pub struct Book {
 
 /// Application.
 #[derive(Debug)]
-pub struct App<'a> {
+pub struct App {
     pub running: bool,
     pub show_popup: bool,
     pub state: TableState,
-    pub items: Vec<Vec<&'a str>>,
+    pub items: Vec<Book>,
 }
 
-impl<'a> Default for App<'a> {
+impl Default for App {
     fn default() -> Self {
         Self {
             running: true,
             show_popup: false,
             state: TableState::default(),
-            items: vec![
-                vec!["Row11", "Row12", "Row13"],
-                vec!["Row21", "Row22", "Row23"],
-                vec!["Row31", "Row32", "Row33"],
-                vec!["Row41", "Row42", "Row43"],
-                vec!["Row51", "Row52", "Row53"],
-                vec!["Row61", "Row62\nTest", "Row63"],
-                vec!["Row71", "Row72", "Row73"],
-                vec!["Row81", "Row82", "Row83"],
-                vec!["Row91", "Row92", "Row93"],
-                vec!["Row101", "Row102", "Row103"],
-                vec!["Row111", "Row112", "Row113"],
-                vec!["Row121", "Row122", "Row123"],
-                vec!["Row131", "Row132", "Row133"],
-                vec!["Row141", "Row142", "Row143"],
-                vec!["Row151", "Row152", "Row153"],
-                vec!["Row161", "Row162", "Row163"],
-                vec!["Row171", "Row172", "Row173"],
-                vec!["Row181", "Row182", "Row183"],
-                vec!["Row191", "Row192", "Row193"],
-            ],
+            items: Vec::new(),
         }
     }
 }
 
-impl<'a> App<'a> {
+impl App {
     /// Constructs a new instance of [`App`].
     pub fn new() -> Self {
         Self::default()
@@ -102,6 +82,9 @@ impl<'a> App<'a> {
     pub fn read_json(&mut self) -> Result<Vec<Book>, Box<dyn error::Error>> {
         let json_content = fs::read_to_string(JSON_PATH)?;
         let parsed: Vec<Book> = serde_json::from_str(&json_content)?;
+        // This is a yucky fix :(
+        // Change by moving to a module so I can use it in default()?
+        self.items = parsed.clone();
         Ok(parsed)
     }
 }
