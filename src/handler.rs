bfd5c18c45f32b2815c1dc98d410467c74efdba7
Aycan <aycan.acn2020@gmail.com>
2023-07-14 19:01:14 +0200

    Disable main keys when adding book

--- src/handler.rs
+++ src/handler.rs
@@ -15,63 +15,66 @@ pub fn change_focus(task: &mut BookState<'_>, forward: bool) -> Result<(), Box<d
 
 /// Handles the key events and updates the state of [`App`].
 pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
-    let updated_task = if let Some(mut task) = app.book_edit_state.take() {
-        match (key_event.code, task.focus) {
-            (KeyCode::Tab, _) => {
-                change_focus(&mut task, true)?;
-                Some(task)
+    if app.book_edit_state.is_some() {
+        let updated_task = if let Some(mut task) = app.book_edit_state.take() {
+            match (key_event.code, task.focus) {
+                (KeyCode::Tab, _) => {
+                    change_focus(&mut task, true)?;
+                    Some(task)
+                }
+                (KeyCode::BackTab, _) => {
+                    change_focus(&mut task, false)?;
+                    Some(task)
+                }
+                (KeyCode::Enter, BookEditFocus::Title) => Some(task),
+                (_, BookEditFocus::Title) => {
+                    task.title.input(key_event);
+                    Some(task)
+                }
+                (_, BookEditFocus::Author) => {
+                    task.author.input(key_event);
+                    Some(task)
+                }
+                _ => Some(task),
             }
-            (KeyCode::BackTab, _) => {
-                change_focus(&mut task, false)?;
-                Some(task)
-            }
-            (KeyCode::Enter, BookEditFocus::Title) => Some(task),
-            (_, BookEditFocus::Title) => {
-                task.title.input(key_event);
-                Some(task)
-            }
-            (_, BookEditFocus::Author) => {
-                task.author.input(key_event);
-                Some(task)
-            }
-            _ => Some(task),
-        }
+        } else {
+            None
+        };
+        app.book_edit_state = updated_task;
     } else {
-        None
-    };
-    app.book_edit_state = updated_task;
-    match key_event.code {
-        // Exit application on `ESC` or `q`
-        KeyCode::Esc | KeyCode::Char('q') => {
-            app.quit();
-        }
-        // Exit application on `Ctrl-C`
-        KeyCode::Char('c') | KeyCode::Char('C') => {
-            if key_event.modifiers == KeyModifiers::CONTROL {
+        match key_event.code {
+            // Exit application on `ESC` or `q`
+            KeyCode::Esc | KeyCode::Char('q') => {
                 app.quit();
             }
-        }
-        // Remove book
-        KeyCode::Char('d') => {
-            if app.items.len() != 0 {
-                app.remove_json_at_index().expect("Failed to remove");
+            // Exit application on `Ctrl-C`
+            KeyCode::Char('c') | KeyCode::Char('C') => {
+                if key_event.modifiers == KeyModifiers::CONTROL {
+                    app.quit();
+                }
             }
-        }
-        KeyCode::Char('a') => {
-            app.book_edit_state = Some(BookState::default());
-            app.show_popup = !app.show_popup;
-        }
-        KeyCode::Up | KeyCode::Char('k') => {
-            if app.items.len() != 0 {
-                app.previous();
+            // Remove book
+            KeyCode::Char('d') => {
+                if app.items.len() != 0 {
+                    app.remove_json_at_index().expect("Failed to remove");
+                }
             }
-        }
-        KeyCode::Down | KeyCode::Char('j') => {
-            if app.items.len() != 0 {
-                app.next();
+            KeyCode::Char('a') => {
+                app.book_edit_state = Some(BookState::default());
+                app.show_popup = !app.show_popup;
             }
+            KeyCode::Up | KeyCode::Char('k') => {
+                if app.items.len() != 0 {
+                    app.previous();
+                }
+            }
+            KeyCode::Down | KeyCode::Char('j') => {
+                if app.items.len() != 0 {
+                    app.next();
+                }
+            }
+            _ => {}
         }
-        _ => {}
     }
     Ok(())
 }
