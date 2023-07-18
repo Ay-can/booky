9008251b682fe5d962e3e78099eed24f5718d642
Aycan <aycan.acn2020@gmail.com>
2023-07-18 20:05:35 +0200

    Add feature that creates a sqlite db and check if there are any pending migrations to run(this was harder then it looks

--- src/database.rs
+++ src/database.rs
@@ -6,8 +6,15 @@ use crate::database::models::*;
 use crate::database::schema::books::dsl::books;
 use crate::database::schema::books::*;
 use diesel::prelude::*;
+use diesel::sqlite::Sqlite;
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+use dirs_2::document_dir;
 use dotenvy::dotenv;
 use std::env;
+use std::error::Error;
+
+const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
+type DB = diesel::sqlite::Sqlite;
 
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
@@ -17,9 +24,24 @@ pub fn establish_connection() -> SqliteConnection {
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
 
+fn run_migrations(
+    connection: &mut impl MigrationHarness<Sqlite>,
+) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+    connection.run_pending_migrations(MIGRATIONS)?;
+    Ok(())
+}
+
+pub fn establish_connection_2() -> SqliteConnection {
+    let database_url = "./books.db";
+    let mut connection =
+        SqliteConnection::establish(database_url).unwrap_or_else(|_| panic!("Error connecting to"));
+    run_migrations(&mut connection);
+    connection
+}
+
 pub fn create_book(new_book: NewBook) -> Book {
     use crate::database::schema::books;
-    let connection = &mut establish_connection();
+    let connection = &mut establish_connection_2();
 
     diesel::insert_into(books::table)
         .values(&new_book)
@@ -30,7 +52,7 @@ pub fn create_book(new_book: NewBook) -> Book {
 
 // Do this without app parameter later
 pub fn get_books(app: &mut App) -> Vec<Book> {
-    let connection = &mut establish_connection();
+    let connection = &mut establish_connection_2();
 
     let results = books
         .select(Book::as_select())
@@ -41,7 +63,7 @@ pub fn get_books(app: &mut App) -> Vec<Book> {
 }
 
 pub fn update_book(book_id: i32, update_book: NewBook) {
-    let connection = &mut establish_connection();
+    let connection = &mut establish_connection_2();
 
     diesel::update(books.find(book_id))
         .set(update_book)
@@ -50,7 +72,7 @@ pub fn update_book(book_id: i32, update_book: NewBook) {
 }
 
 pub fn delete_book(app: &mut App) {
-    let connection = &mut establish_connection();
+    let connection = &mut establish_connection_2();
     if let Some(selected) = app.state.selected() {
         let current_id = app.items.get(selected).unwrap().id;
         app.items.remove(selected);
